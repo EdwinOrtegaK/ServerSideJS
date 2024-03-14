78d1df76ff22ae48612c5efd69058c9d8e0b9339
Edwin Ortega <andyortegakou@yahoo.com.mx>
2024-03-13 20:28:41 -0600

    refactor: not null added to variables

--- schema.sql
+++ schema.sql
@@ -1,15 +1,17 @@
 CREATE USER IF NOT EXISTS 'andyortegak'@'%' IDENTIFIED BY 'kou12345';
-GRANT ALL PRIVILEGES ON blog_db.* TO 'blog_user'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON blog_db.* TO 'andyortegak'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+
+CREATE DATABASE IF NOT EXISTS blog_db;
 
 CREATE TABLE IF NOT EXISTS blog_posts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
-    pokemon_name VARCHAR(255),
-    tipo_pokemon VARCHAR(255),
-    grupo VARCHAR(255),
-    cualidad VARCHAR(255),
-    region VARCHAR(255),
-    descripción VARCHAR(255)
+    pokemon_name VARCHAR(255) NOT NULL,
+    tipo_pokemon VARCHAR(255) NOT NULL,
+    grupo VARCHAR(255) NOT NULL,
+    cualidad VARCHAR(255) NOT NULL,
+    region VARCHAR(255) NOT NULL,
+    descripcion VARCHAR(255) NOT NULL
 );
