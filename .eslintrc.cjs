66f182baba77dba1b3681fce44c65d5f3e80ce20
Edwin Ortega <andyortegakou@yahoo.com.mx>
2024-03-14 15:06:31 -0600

    feat: new rules for lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,5 +23,14 @@ module.exports = {
   },
   rules: {
     "semi": ["error", "never"], // Prohibir el uso de punto y coma
+    "object-curly-newline": "off", //Desactivar object-curly-newline por conflicto
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "js": "ignorePackages" //Set import/extensions to never because of ERR_MODULE_NOT_FOUND when eliminating ".js" extensions from file imports
+      }
+    ],
+    "no-console": 0
   },
 };
